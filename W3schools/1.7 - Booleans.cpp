35ad1217e5dcccb5fea8a8976c7f7dbb7466ad69
Lukashatirishvili <126322793+Lukashatirishvili@users.noreply.github.com>
2023-03-04 12:13:19 +0400

    Add files via upload

---
+++ W3schools/1.7 - Booleans.cpp
@@ -0,0 +1,17 @@
+#include <iostream>;
+
+using namespace std;
+
+
+int main() {
+    // Boolean expression
+    int x = 4;
+    int y = 7;
+    cout << (x < y) << endl;
+    cout << (x == 4) << endl;
+
+    int myAge = 17;
+    int votingAge = 18;
+
+    cout << (myAge >= votingAge);
+}
