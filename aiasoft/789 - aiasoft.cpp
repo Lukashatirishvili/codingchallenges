d79827379577aa18fdfa7f0548237c04e6c36838
Lukashatirishvili <126322793+Lukashatirishvili@users.noreply.github.com>
2023-03-07 23:36:37 +0400

    Add files via upload

---
+++ aiasoft/789 - aiasoft.cpp
@@ -0,0 +1,20 @@
+#include <iostream>;
+
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+
+    int x[n];
+    for (int i = 0; i < n; i++) {
+        cin >> x[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (x[i] % 2 == 1) x[i] += 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << x[i] << " ";
+    }
+}
