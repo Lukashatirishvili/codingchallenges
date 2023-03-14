20a796d28471e5666d1948cf6cfe65b7422bf077
Lukashatirishvili <126322793+Lukashatirishvili@users.noreply.github.com>
2023-03-14 20:53:14 +0400

    Update 88 - aiasoft.cpp
    https://www.aiasoft.ge/problem/88

--- aiasoft/88 - aiasoft.cpp
+++ aiasoft/88 - aiasoft.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+// https://www.aiasoft.ge/problem/88
 
 int main() {
     int n; cin >> n;
+    
     if (n % 2 == 0) {
         cout << "yes";
     } else cout << "no";
