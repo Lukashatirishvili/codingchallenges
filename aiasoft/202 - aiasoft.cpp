d1b7708c72b7fd1267446f5365a8dd31e28cbce4
Lukashatirishvili <126322793+Lukashatirishvili@users.noreply.github.com>
2023-03-23 11:44:43 +0400

    Add files via upload
    https://aiasoft.ge/problem/202

---
+++ aiasoft/202 - aiasoft.cpp
@@ -0,0 +1,24 @@
+#include <iostream>;
+
+using namespace std;
+
+int main () {
+    int n; cin >> n;
+    long long a[n + 1];
+
+    long long d[n + 1];
+
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+
+    d[1] = a[1];
+    d[2] = a[2];
+
+    for (int i = 3; i <= n; i++) {
+        d[i] = min(d[i - 1], d[i - 2]) + a[i];
+    }
+
+    cout << d[n];
+
+}
