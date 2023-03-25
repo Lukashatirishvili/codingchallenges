2a95335ec1386c1311f9aac4bfcbb812f66b4ad1
Lukashatirishvili <126322793+Lukashatirishvili@users.noreply.github.com>
2023-03-25 11:51:14 +0400

    Add files via upload
    https://www.aiasoft.ge/problem/248

---
+++ aiasoft/248 - aiasoft.cpp
@@ -0,0 +1,25 @@
+#include <iostream>;
+
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    int a[n];
+    int temp;
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+}
