ec7e39fe4e1c934a0f0beee84893dad34ecf7831
Lukashatirishvili <126322793+Lukashatirishvili@users.noreply.github.com>
2023-03-25 12:41:31 +0400

    Add files via upload
    https://www.aiasoft.ge/problem/126

---
+++ aiasoft/126 - aiasoft.cpp
@@ -0,0 +1,18 @@
+#include <iostream>;
+
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+
+    long long a[n + 1];
+
+    a[1] = 1;
+    a[2] = 1;
+
+    for (int i = 3; i <= n; i++) {
+        a[i] = a[i - 1] + a[i - 2];
+    }
+
+    cout << a[n];
+}
