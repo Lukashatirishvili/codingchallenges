303859d24eaec98d5c36629db667978a7ff47c6d
Lukashatirishvili <126322793+Lukashatirishvili@users.noreply.github.com>
2023-04-28 12:13:13 +0400

    Add files via upload
    https://codeforces.com/problemset/problem/1692/A

---
+++ Codeforces/1692A - Marathon.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t, a, b, c, d;
+    cin >> t;
+
+    int count = 0;
+
+    while(t--) {
+        cin >> a >> b >> c >> d;
+
+        if (b > a) count++;
+        if (c > a) count++;
+        if (d > a) count++;
+
+        cout << count << " ";
+
+        count = 0;
+    }
+
+}
